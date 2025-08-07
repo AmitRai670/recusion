67e6e2569eb095d63f3caa93fc1a889ad2dd72ca
AmitRai670 <8387amit@gmail.com>
2025-08-08 00:30:26 +0530

    Update Permutaion.java

--- Permutaion.java
+++ Permutaion.java
@@ -1,4 +1,57 @@
 package Com.Recursion.Subset;
 
+import java.util.ArrayList;
+
 public class Permutaion {
+    public static void main(String[] args){
+//        permutaion("","abc");
+//        ArrayList<String> ans = permutaionList("","abc");
+//        System.out.println(ans);
+        System.out.println(permutaionCOunt("","abc"));
+    }
+    static void permutaion(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+        char ch =up.charAt(0);
+
+        for(int i = 0; i<=p.length();i++){
+            String f = p.substring(0,i);
+            String s = p.substring(i,p.length());
+            permutaion(f+ch+s,up.substring(1));
+        }
+    }
+    static ArrayList<String> permutaionList(String p, String up){
+        if(up.isEmpty()){
+           ArrayList<String> list = new ArrayList<>();
+           list.add(p);
+           return list;
+        }
+        char ch =up.charAt(0);
+
+        ArrayList<String> ans = new ArrayList<>();
+        for(int i = 0; i<=p.length();i++){
+            String f = p.substring(0,i);
+            String s = p.substring(i,p.length());
+           ans.addAll(permutaionList(f+ch+s,up.substring(1)));
+
+        }
+        return ans;
+    }
+
+    static int permutaionCOunt(String p, String up){
+        if(up.isEmpty()){
+            return 1;
+        }
+        int count =0;
+        char ch =up.charAt(0);
+
+        for(int i = 0; i<=p.length();i++){
+            String f = p.substring(0,i);
+            String s = p.substring(i,p.length());
+           count=count + permutaionCOunt(f+ch+s,up.substring(1));
+        }
+        return count;
+    }
 }
